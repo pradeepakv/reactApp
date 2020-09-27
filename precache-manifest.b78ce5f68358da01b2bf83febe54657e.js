1367b030762825a6a4f1eccfa930016a5212043f
Kathiresan, Pradeepa <pradeepa.kathiresan@accenture.com>
2020-09-27 20:24:57 +0530

    Updates

---
+++ precache-manifest.b78ce5f68358da01b2bf83febe54657e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d36d6f676eda40214f6e01264fb95c",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "a2956f50f22ad8d1b721",
+    "url": "/reactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/reactApp/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2956f50f22ad8d1b721",
+    "url": "/reactApp/static/js/main.f69b8c64.chunk.js"
+  },
+  {
+    "revision": "3827da42033a28b9643e",
+    "url": "/reactApp/static/js/runtime-main.b77f6c20.js"
+  }
+]);
